2c12230613c4e37101d9073f175942d95b5e45be
RM7370RF <rm7370rf@yandex.ru>
2019-05-28 20:38:50 +0300

    Add toLower() function

--- StringUtils.sol
+++ StringUtils.sol
@@ -8,4 +8,20 @@ library StringUtils {
     function isEmpty(string self) internal pure returns(bool) {
         return (bytes(self).length == 0);
     }
+
+    function toLower(string self) internal pure returns (string) {
+        //based on https://gist.github.com/ottodevs/c43d0a8b4b891ac2da675f825b1d1dbf
+        bytes memory bStr = bytes(self);
+        bytes memory bLower = new bytes(bStr.length);
+        for (uint i = 0; i < bStr.length; i++) {
+            // Uppercase character...
+            if ((bStr[i] >= 65) && (bStr[i] <= 90)) {
+                // So we add 32 to make it lowercase
+                bLower[i] = bytes1(int(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        return string(bLower);
+    }
 }
